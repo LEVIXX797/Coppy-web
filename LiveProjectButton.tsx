efd5b467e4702aa4af7b46f036d119875332f60d
LEVIXX797 <lovejotmaan786@gmail.com>
2026-06-04 16:50:25 +0530

    Create LiveProjectButton.tsx

---
+++ LiveProjectButton.tsx
@@ -0,0 +1,19 @@
+export default function LiveProjectButton() {
+  return (
+    <button
+      className="
+      rounded-full
+      border-2
+      border-[#D7E2EA]
+      text-[#D7E2EA]
+      px-8 py-3
+      sm:px-10 sm:py-3.5
+      uppercase
+      tracking-[0.2em]
+      hover:bg-[#D7E2EA]/10
+      transition"
+    >
+      Live Project
+    </button>
+  );
+}
